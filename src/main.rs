5304bb8fa800e5497905602ee2298472c767eaa4
DocRAID <l01062506145@gmail.com>
2022-12-18 20:34:25 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::PathBuf;
 
+
 use actix_multipart::Multipart;
 use actix_files::Files;
 use actix_web::*;
@@ -10,6 +11,7 @@ use std::io::{Write, BufReader, BufRead, Error};
 use calamine::{Reader, Xlsx, open_workbook};
 use serde::Serialize;
 
+
 const HOST:(&str,u16) = ("127.0.0.1",8082);
 
 #[derive(Serialize)]
@@ -17,12 +19,15 @@ struct UserData {
     id:i16
 }
 
+
+
 fn next_filename() -> Option<PathBuf> {
     let mut p = PathBuf::new();
     let time:String = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().to_string();
     p.push(format!("uploaded-excel/{}.xlsx",time));
     Some(p)
 }
+
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("200")
@@ -42,6 +47,7 @@ async fn trance_ex_to_js(req_excel: HttpRequest) -> web::Json<UserData> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Running at http://{}:{}",HOST.0,HOST.1);
     HttpServer::new(|| {
         App::new()
             .service(hello)
