14ed93ad7430e3e7596fd28f5a80a1a7064353df
jinhyo-dev <jinhyo0850@gmail.com>
2022-12-26 13:49:30 +0900

    struct UserData variable name changed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use actix_cors::Cors;
 use actix_easy_multipart::MultipartForm;
 use actix_easy_multipart::tempfile::Tempfile;
@@ -6,20 +5,19 @@ use actix_web::{get, post,web, App, HttpResponse, HttpServer, Responder, http};
 use calamine::{Reader, Xlsx, open_workbook};
 use serde::Serialize;
 
-
 const HOST:(&str,u16) = ("127.0.0.1",8082);
 
 #[derive(Serialize)]
 struct UserData {
-    position:i16,
+    role:i16,
     name:String,
     grade:Option<i8>,
     class:Option<i8>,
     number:Option<i8>,
     phone:String,
-    m_phone1:Option<String>,
-    m_phone2:Option<String>,
-    id:String,
+    parentsPhoneNum1:Option<String>,
+    parentsPhoneNum2:Option<String>,
+    account:String,
     password:String
 }
 
@@ -52,17 +50,16 @@ async fn trance_student_ex_to_js(req_excel: MultipartForm<Form>) -> web::Json<Ve
     let mut result:Vec<UserData> = Vec::new();
     if let Some(Ok(r)) = excel.worksheet_range("Sheet1") {
         for row in r.rows() {
-            // println!("row={:?}", row);
             result.push(UserData {
-                position: row[0].get_float().unwrap() as i16,
+                role: row[0].get_float().unwrap() as i16,
                 name:row[1].to_string(),
                 grade:Some(row[2].get_float().unwrap() as i8),
                 class:Some(row[3].get_float().unwrap() as i8),
                 number:Some(row[4].get_float().unwrap() as i8),
                 phone:row[5].to_string(),
-                m_phone1:Some(row[6].to_string()),
-                m_phone2:Some(row[7].to_string()),
-                id:row[8].to_string(),
+                parentsPhoneNum1:Some(row[6].to_string()),
+                parentsPhoneNum2:Some(row[7].to_string()),
+                account:row[8].to_string(),
                 password:row[9].to_string()
             });
         }
